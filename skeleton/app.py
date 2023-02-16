b41baf2971aeee7c19fe1c1cf129284dd81978fb
Codelixx <codelix@outlook.de>
2023-02-16 23:23:24 +0100

    add another example

--- skeleton/app.py
+++ skeleton/app.py
@@ -4,6 +4,8 @@ from dash import html, Output, Input, callback
 import pandas as pd
 from time import sleep
 
+from components.cards import create_card
+
 app = Dash(
     __name__,
     suppress_callback_exceptions=True,
@@ -21,7 +23,37 @@ app.layout = html.Div(children=[
                 dmc.Button("Click Me!", id="datatable-skeleton-button"),
             ],
             style={"padding": "32px"}
-        )
+        ),
+        html.Div(
+            [
+                dmc.Skeleton(
+                    visible=False,
+                    height=500,
+                    width=475,
+                    children=html.Div(id="skeleton-card-1-container"),
+                ),
+                dmc.Skeleton(
+                    visible=False,  
+                    height=500,
+                    width=475,
+                    children=html.Div(id="skeleton-card-2-container"),
+                ),
+                dmc.Skeleton(
+                    visible=False,
+                    height=500,
+                    width=475,
+                    children=html.Div(id="skeleton-card-3-container"),
+                ),
+                dmc.Skeleton(
+                    visible=False,
+                    height=500,
+                    width=475,
+                    children=html.Div(id="skeleton-card-4-container"),
+                ),
+            ],
+            style={"padding": "32px", "display": "flex", "justifyContent": "space-between"}
+        ),
+        dmc.Button("Click Me!", id="card-skeleton-button"),
     ]
 )
 
@@ -50,6 +82,42 @@ def create_table(n_clicks):
     )
     sleep(3)
     return table
+
+
+@callback(
+    Output("skeleton-card-1-container", "children"),
+    Output("skeleton-card-2-container", "children"),
+    Output("skeleton-card-3-container", "children"),
+    Output("skeleton-card-4-container", "children"),
+    Input("card-skeleton-button", "n_clicks")
+)
+def create_table(n_clicks):
+    card_1 = create_card(
+        image_src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
+        title="Dash is Awesome!",
+        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l"
+    )
+        
+    card_2 = create_card(
+        image_src="https://images.unsplash.com/photo-1608222351212-18fe0ec7b13b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1548&q=80",
+        title="DMC is Awesome!",
+        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l"
+    )
+
+    card_3 = create_card(
+        image_src="https://images.unsplash.com/photo-1526379095098-d400fd0bf935?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2064&q=80",
+        title="Python as well!",
+        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l"
+    )
+
+    card_4 = create_card(
+        image_src="https://images.unsplash.com/photo-1515871204537-49a5fe66a31f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1564&q=80",
+        title="Do not forget to follow me <3",
+        text="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut l"
+    )
+    sleep(3)
+
+    return card_1, card_2, card_3, card_4
 
 if __name__ == '__main__':
     app.run_server(debug=True)
